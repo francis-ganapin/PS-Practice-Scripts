e39a83c1cd39e29c393f16b987778afb0a3294f7
Francis <83027350+francis-ganapin@users.noreply.github.com>
2021-11-05 21:22:56 +0800

    added comment test1
    added comment #test1

--- arraysample.ps1
+++ arraysample.ps1
@@ -2,7 +2,7 @@
 #for (define the the variable we want to use to count and it's value; what kind of condition we will use when were counting; 
  # either increase or decrease the variable) { command we want to repeat
  #}
-#test
+#test1
 $MyArray = @("cars","Trucks","MotorCycles","SUVs")
 for ($i=0; $i -lt 4; $i++) {
 echo ("Element $i value: " + $MyArray[$i])
